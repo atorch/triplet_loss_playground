8229cf99efa28f2f9650c2236f64c8f9e48654f2
Adrian Torchiana <adrian.torchiana@gmail.com>
2022-04-28 20:43:36 -0700

    Try training for more epochs and with more filters

--- README.md
+++ README.md
@@ -10,3 +10,40 @@ mkdir -p ~/triplet_loss_playground/tfds_data
 sudo docker run -u $(id -u):$(id -g) --gpus all -it -v ~/triplet_loss_playground:/home/triplet_loss_playground triplet_loss_playground bash
 python src/fit_model.py
 ```
+
+```
+Epoch 200/200
+1875/1875 [==============================] - 28s 15ms/step - loss: 0.1982
+Sanity check: are all model outputs unit vectors, as expected?
+True
+```
+
+```
+Distances between my hand drawn Xs:
+[[0.         1.14174511 0.79931824]
+ [1.14174511 0.         0.68824712]
+ [0.79931824 0.68824712 0.        ]]
+Distances between mnist digits and my hand drawn Xs:
+min=0.3518602813396214, mean=1.3891414920301075, median=1.4871171013090958, max=1.9194239968854707
+Min distances between mnist digits and my hand drawn Xs:
+[0.45689708 0.35186028 0.3848151 ]
+Classes of the mnist digits closest to my hand drawn Xs:
+[6 8 8]
+```
+
+```
+Distances between my hand drawn 8s:
+[[0.         0.71040955 0.54930204]
+ [0.71040955 0.         0.59276718]
+ [0.54930204 0.59276718 0.        ]]
+Distances between mnist digits and my hand drawn 8s:
+min=0.19103638002534976, mean=1.3873805569013555, median=1.4724980990818684, max=1.8657132767900848
+Min distances between mnist digits and my hand drawn 8s:
+[0.19740385 0.53029324 0.19103638]
+Classes of the mnist digits closest to my hand drawn 8s:
+[8 8 8]
+Distances between my hand-drawn 8s and Xs:
+[[1.07054016 0.50638428 0.59712251]
+ [1.409789   0.62809385 1.11967489]
+ [1.23026855 0.50905983 0.78410262]]
+```
--- src/fit_model.py
+++ src/fit_model.py
@@ -7,12 +7,12 @@ import tensorflow_addons as tfa
 import tensorflow_datasets as tfds
 
 
-def get_model(embedding_dim=128):
+def get_model(embedding_dim, n_filters1=128, n_filters2=64, dropout=0.2):
 
     model = tf.keras.Sequential(
         [
             tf.keras.layers.Conv2D(
-                filters=64,
+                filters=n_filters1,
                 kernel_size=2,
                 padding="same",
                 activation="relu",
@@ -21,10 +21,10 @@ def get_model(embedding_dim=128):
             tf.keras.layers.MaxPooling2D(pool_size=2),
             tf.keras.layers.Dropout(0.3),
             tf.keras.layers.Conv2D(
-                filters=32, kernel_size=2, padding="same", activation="relu"
+                filters=n_filters2, kernel_size=2, padding="same", activation="relu"
             ),
             tf.keras.layers.MaxPooling2D(pool_size=2),
-            tf.keras.layers.Dropout(0.3),
+            tf.keras.layers.Dropout(dropout),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(
                 embedding_dim, activation=None
@@ -40,15 +40,34 @@ def get_model(embedding_dim=128):
         loss=tfa.losses.TripletSemiHardLoss(),
     )
 
+    print(model.summary())
+
     return model
 
 
 def _normalize_img(img, label):
+
+    # Normalize values that are originally in [0, 1, ..., 2^8 - 1]
     img = tf.cast(img, tf.float32) / 255.0
     return (img, label)
 
 
-def main(n_epochs=50):
+def describe_distances(predictions_on_new_images, new_image_description, predictions_on_digits, test_classes):
+
+    print("***")
+    print(f"Distances between {new_image_description}:")
+    print(distance_matrix(predictions_on_new_images, predictions_on_new_images))
+
+    distances = distance_matrix(predictions_on_new_images, predictions_on_digits)
+    print(f"Distances between mnist digits and {new_image_description}:")
+    print(f"min={distances.min()}, mean={distances.mean()}, median={np.median(distances)}, max={distances.max()}")
+    print(f"Min distances between mnist digits and {new_image_description}:")
+    print(np.min(distances, axis=1))
+    print(f"Classes of the mnist digits closest to {new_image_description}:")
+    print(test_classes[np.argmin(distances, axis=1)])
+
+
+def main(n_epochs=200, embedding_dim=64):
 
     train_dataset, test_dataset = tfds.load(
         name="mnist", split=["train", "test"], as_supervised=True
@@ -57,8 +76,9 @@ def main(n_epochs=50):
     test_as_np = tfds.as_numpy(test_dataset)
     for image, label in test_as_np:
         imageio.imwrite("example_mnist_digit.png", image)
-        break
+        break  # TODO Save a few more?  Not the full training set, maybe the first batch
 
+    # TODO Try adding some random shifts and small rotations to the training set?  Visualize them
     train_dataset = train_dataset.shuffle(1024).batch(32)
     train_dataset = train_dataset.map(_normalize_img)
 
@@ -66,54 +86,47 @@ def main(n_epochs=50):
     test_dataset = test_dataset.map(_normalize_img)
     test_classes = np.concatenate([y for x, y in test_dataset], axis=0)
 
-    # TODO Try embedding_dim=64?
-    model = get_model()
+    model = get_model(embedding_dim=embedding_dim)
 
     history = model.fit(train_dataset, epochs=n_epochs,)
 
     # Maps images of digits into embedding_dim
     predictions_on_digits = model.predict(test_dataset)
 
-    # These are Xs, i.e. they're a type of image that was never seen during training
+    print("Sanity check: are all model outputs unit vectors, as expected?")
+    print(np.allclose(np.linalg.norm(predictions_on_digits, axis=1), 1.0))
+
+    # These are Xs, a shape that was never seen during training
     new_image1 = imageio.imread("new_images/x1.png")
     new_image2 = imageio.imread("new_images/x2.png")
     new_image3 = imageio.imread("new_images/x3.png")
     new_images = np.stack([new_image1, new_image2, new_image3])
-
     # Match the model's expected input shape
     new_images = np.expand_dims(new_images, axis=-1)
-
     predictions_on_Xs = model.predict(_normalize_img(new_images, label=None)[0])
 
-    # We'd like these to be nearby (small distance), are they?
-    print("Distances between new images (images of Xs):")
-    print(np.linalg.norm(predictions_on_Xs[0] - predictions_on_Xs[1]))
-    print(np.linalg.norm(predictions_on_Xs[0] - predictions_on_Xs[2]))
-    print(np.linalg.norm(predictions_on_Xs[1] - predictions_on_Xs[2]))
-
-    # Expect these to be far (larger distance)
-    print("Distances between new images of Xs and digits (min, mean, max):")
-    # print(np.linalg.norm(predictions_on_Xs[0] - predictions_on_digits[0]))  # Should match distances[0, 0]
-    # TODO Save a histogram of these distances?
-    distances = distance_matrix(predictions_on_Xs, predictions_on_digits)
-    print(f"{distances.min()}, {distances.mean()}, {distances.max()}")
+    describe_distances(
+        predictions_on_new_images=predictions_on_Xs,
+        new_image_description="my hand drawn Xs",
+        predictions_on_digits=predictions_on_digits,
+        test_classes=test_classes,
+    )
 
     # These are 8s that I drew by hand, so they should be close to the 8s from mnist, but let's check
     my_eight1 = imageio.imread("new_images/an_eight_not_from_mnist1.png")
     my_eight2 = imageio.imread("new_images/an_eight_not_from_mnist2.png")
     my_eight3 = imageio.imread("new_images/an_eight_not_from_mnist3.png")
     my_eights = np.stack([my_eight1, my_eight2, my_eight3])
-
     # Match the model's expected input shape
     my_eights = np.expand_dims(my_eights, axis=-1)
-
     predictions_on_eights = model.predict(_normalize_img(my_eights, label=None)[0])
-    distances = distance_matrix(predictions_on_eights, predictions_on_digits)
 
-    print("Min distances between mnist digits and my hand-drawn 8s:")
-    print(np.min(distances, axis=1))
-    print("Classes of the mnist digits closest to my hand-drawn 8s:")
-    print(test_classes[np.argmin(distances, axis=1)])
+    describe_distances(
+        predictions_on_new_images=predictions_on_eights,
+        new_image_description="my hand drawn 8s",
+        predictions_on_digits=predictions_on_digits,
+        test_classes=test_classes,
+    )
 
     print("Distances between my hand-drawn 8s and Xs:")
     print(distance_matrix(predictions_on_eights, predictions_on_Xs))
